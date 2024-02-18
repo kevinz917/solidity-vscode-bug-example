fa76f0ef598795c6ab60fd62389e753eeb6671a1
Kevin Z <zkw917@gmail.com>
2024-02-18 14:11:11 -0800

    a

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract Counter is ERC20 {
     uint256 public number;
 
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol, 18) {}
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol, 18) {}
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;

ecd358e9443f1c5e81203f1b8b2aef139b6eaafc
Kevin Z <zkw917@gmail.com>
2024-02-19 01:31:33 -0800

    new

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
